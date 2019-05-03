b61c21f30920dbafd52bd7e1cbf8aacdb1e549ed
Jacob Angulo <jacobangulo@hotmail.com>
2019-05-03 13:58:20 -0700

    declared client id and domain for linkage to auth 0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import Auth0Lock from "auth0-lock";
 
-const lock = new Auth0Lock("YOUR_CLIENT_ID", "YOUR_DOMAIN", {
-  auth: {
-    redirectUrl: "https://flamboyant-mcclintock-855856.netlify.com/",
-    responseType: "code",
-    params: {
-      scope: "openid email" // Learn about scopes: https://auth0.com/docs/scopes
+const lock = new Auth0Lock(
+  "yAYhoHJ0U3Ie0GDf09Auo1qiQDIpV5yM", // auth 0 client id
+  "dev-pvcx3ufu.auth0.com", // auth 0 domain
+  {
+    auth: {
+      redirectUrl: "https://flamboyant-mcclintock-855856.netlify.com/",
+      responseType: "code",
+      params: {
+        scope: "openid email" // Learn about scopes: https://auth0.com/docs/scopes
+      }
     }
   }
-});
+);
 
 function App() {
   return (
